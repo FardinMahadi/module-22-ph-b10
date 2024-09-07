8f0d55ac53211bf5a520b1141ab9bc3b2e1d3355
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-09-07 07:30:07 +0600

    starting

--- problem1.js
+++ problem1.js
@@ -2,14 +2,13 @@ function inchToFeet(inch) {
   if (inch % 12 == 0) {
     return `${inch / 12}'`;
   } else {
-    let feet = Math.floor(inch / 12);
-    return `${feet}'${inch % 12}"`;
+    return `${Math.floor(inch / 12)}'${inch % 12}"`;
   }
 }
 
-function mileToKilometer(mile) {
+function mileToKiloMeter(mile) {
   return mile * 1.609344;
 }
 
 console.log(inchToFeet(66));
-console.log(mileToKilometer(5));
+console.log(mileToKiloMeter(5));
