7a0e2db064053f67ee9564bdb3366b6e33465a85
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-09-07 07:19:57 +0600

    done

---
+++ problem1.js
@@ -0,0 +1,10 @@
+function inchToFeet(inch) {
+  return inch / 12;
+}
+
+function mileToKilometer(mile) {
+  return mile * 1.609344;
+}
+
+console.log(inchToFeet(60));
+console.log(mileToKilometer(5));
