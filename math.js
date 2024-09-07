59812f345322e8b581d3a11be4fa03bbfabb1869
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-09-07 09:04:55 +0600

    done

---
+++ math.js
@@ -0,0 +1,23 @@
+// const Math = {
+//   min: function min(num1, num2) {
+//     return num2;
+//   },
+// };
+
+// console.log(Math.min);
+
+const min = Math.min(53, 0.251, 452, 542, 64, 642, 32);
+const max = Math.max(53, 0.251, 452, 542, 64, 642, 32);
+
+console.log(min);
+console.log(max);
+
+console.log(Math.PI);
+console.log(Math.abs(-12));
+
+console.log(Math.round(329.143));
+
+console.log(Math.floor(2.4324342));
+console.log(Math.ceil(2.4324342));
+
+console.log(Math.round(Math.random() * 10));
